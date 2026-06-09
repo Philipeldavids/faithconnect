8d1481166a37646fc8c0ab8d14f81b0bda03418d
Philip El Davids <olorunfemiayanfe@gmail.com>
2026-06-09 17:33:28 +0100

    edit baseurl

--- faithconnect/src/lib/api/axios.ts
+++ faithconnect/src/lib/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: import.meta.env.VITE_API_URL ?? "https://localhost:44359/api"
-  baseURL: "https://ghconnect.onrender.com/"
+  baseURL: "https://ghconnect.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
